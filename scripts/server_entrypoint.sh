e92281b482350fd4bc9f2048240b7538ee0d36fc
Marius <marius.schulze@solutions.work>
2020-11-15 20:17:06 +0100

    adjust and test out deployment script

--- scripts/server_entrypoint.sh
+++ scripts/server_entrypoint.sh
@@ -2,13 +2,6 @@
 
 # wait for postgres to become available before running migrations
 
-while ! nc -z $SQL_HOST $SQL_PORT; do
-  >&2 echo "Postgres is unavailable - sleeping"
-  sleep 1
-done
-
-
-# once postgres is up, sleep for a few seconds to give it time to fully initialize
 sleep 3
 
 su app -s /bin/sh -c "python3 ./manage.py migrate --noinput"
